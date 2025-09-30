503e284332232f74642bdfbb46368560bc425688
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2025-09-30 18:23:29 -0300

    pantalla login y register

--- usuario/src/app/app.routes.ts
+++ usuario/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login';
+import {RegisterComponent} from "./pages/register/register";
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent}
+];
