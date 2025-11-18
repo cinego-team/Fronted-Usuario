fa20b5dc317aae0d8d08c0dcde39dbd86b12ea72
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2025-11-18 15:35:59 -0300

    seguridad del login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,27 +3,46 @@ import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../axios_service/auth.service';
 import { TokenTimeoutService } from '../axios_service/tokenTimeout.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
+  formulario: FormGroup;
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
 
-  email: string = '';
-  password: string = '';
-
-  constructor(private authService: AuthService, private router: Router, private tokenTimeoutService: TokenTimeoutService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private tokenTimeoutService: TokenTimeoutService,
+    private fb: FormBuilder
+  ) {
+    this.formulario = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
   OnLogin() {
-    this.authService.login({ email: this.email, password: this.password }).then(() => {
-      this.tokenTimeoutService.startCountdown();
-      this.router.navigate(['/cartelera']);
-    }).catch(error => {
-      alert('Login fallido. Verifica tus credenciales.');
+    if (this.formulario.invalid) {
+      this.errorMessage = 'Por favor, completa todos los campos correctamente.';
+      this.formulario.markAllAsTouched();
+      return;
     }
-    )
-  };
+    this.authService
+      .login({ email: this.formulario.value.email, password: this.formulario.value.password })
+      .then(() => {
+        this.tokenTimeoutService.startCountdown();
+        this.router.navigate(['/cartelera']);
+      })
+      .catch((error) => {
+        alert('Login fallido. Verifica tus credenciales.');
+      });
+  }
 }
