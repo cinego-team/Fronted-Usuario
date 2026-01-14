10d83bf0f7cccd734861865bdc265c45fc40f7c4
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2026-01-13 23:22:36 -0300

    depuracion

--- src/app/pages/pelicula/pelicula.ts
+++ src/app/pages/pelicula/pelicula.ts
@@ -4,94 +4,94 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../axios_service/api.service';
 import { GlobalStatusService } from '../axios_service/global-status.service';
 import { Pelicula } from '../cartelera/cartelera';
-
+import { Header } from '../../shared/header/header';
+interface Idioma {
+  id: number;
+  nombre: string;
+}
 export interface Funcion {
+  id: number;
+  estaDisponible: boolean;
+  fecha: Date;
+  idioma: {
     id: number;
-    estaDisponible: boolean;
-    fecha: Date;
-    idioma: {
-        id: number;
-        nombre: string;
-    };
-    sala: {
-        id: number;
-        nroSala: number;
-    }
-    formato: {
-        id: number;
-        nombre: string;
-        precio: number;
-    }
+    nombre: string;
+  };
+  sala: {
+    id: number;
+    nroSala: number;
+  };
+  formato: {
+    id: number;
+    nombre: string;
+    precio: number;
+  };
 }
 
 @Component({
-    selector: 'app-pelicula',
-    standalone: true,
-    imports: [CommonModule],
-    templateUrl: './pelicula.html',
-    styleUrls: ['./pelicula.css'],
+  selector: 'app-pelicula',
+  standalone: true,
+  imports: [CommonModule, Header],
+  templateUrl: './pelicula.html',
+  styleUrls: ['./pelicula.css'],
 })
-
 export class PeliculaComponent implements OnInit {
-    pelicula: Pelicula | null = null;
-    funciones: Funcion[] = [];
-    funcionSeleccionada: Funcion | null = null;
-    mostrarResumen = false;
+  pelicula: Pelicula | null = null;
+  funciones: Funcion[] = [];
+  funcionSeleccionada: Funcion | null = null;
+  mostrarResumen = false;
 
-    constructor(
-        private router: Router,
-        private readonly apiService: ApiService,
-        private readonly globalStatusService: GlobalStatusService
-    ) { }
+  constructor(
+    private router: Router,
+    private readonly apiService: ApiService,
+    private readonly globalStatusService: GlobalStatusService
+  ) {}
 
-    ngOnInit(): void {
-        const navigation = this.router.getCurrentNavigation();
-        const pelicula =
-            navigation?.extras?.state?.['pelicula'] ??
-            history.state?.pelicula ??
-            null;
-        this.pelicula = pelicula;
-        this.initialization()
+  ngOnInit(): void {
+    const navigation = this.router.getCurrentNavigation();
+    const pelicula = navigation?.extras?.state?.['pelicula'] ?? history.state?.pelicula ?? null;
+    this.pelicula = pelicula;
+    this.initialization();
+  }
+
+  async initialization(): Promise<void> {
+    this.globalStatusService.setLoading(true);
+    if (!this.pelicula) {
+      throw new Error('Ocurrió un error al cargar la película.');
     }
-
-    async initialization(): Promise<void> {
-        this.globalStatusService.setLoading(true);
-        if (!this.pelicula) {
-            throw new Error('Ocurrió un error al cargar la película.');
-        }
-        const funcionesBack = await this.apiService.getFuncionesByPeliculaId(this.pelicula.id);
-        if (funcionesBack.length === 0) {
-            alert('No hay funciones para mostrar.');
-            this.globalStatusService.setLoading(false);
-            return;
-        }
-        this.funciones = funcionesBack;
-        this.globalStatusService.setLoading(false);
+    const funcionesBack = await this.apiService.getFuncionesByPeliculaId(this.pelicula.id);
+    if (funcionesBack.length === 0) {
+      alert('No hay funciones para mostrar.');
+      this.globalStatusService.setLoading(false);
+      return;
     }
+    this.funciones = funcionesBack;
+    this.globalStatusService.setLoading(false);
+  }
 
-    seleccionarFuncion(funcion: Funcion): void {
-        this.funcionSeleccionada = funcion;
-        this.mostrarResumen = true;
-    }
+  seleccionarFuncion(funcion: Funcion): void {
+    this.funcionSeleccionada = funcion;
+    this.mostrarResumen = true;
+  }
 
-    cancelarResumen(): void {
-        this.mostrarResumen = false;
-        this.funcionSeleccionada = null;
-    }
+  cancelarResumen(): void {
+    this.mostrarResumen = false;
+    this.funcionSeleccionada = null;
+  }
 
-    confirmarCompra(): void {
-        if (this.pelicula && this.funcionSeleccionada) {
-            this.mostrarResumen = false;
-            this.router.navigate(['/seleccion-butaca'], {
-                state: {
-                    funcion: this.funcionSeleccionada,
-                    pelicula: this.pelicula
-                },
-            });
-        }
+  confirmarCompra(): void {
+    if (this.pelicula && this.funcionSeleccionada) {
+      this.mostrarResumen = false;
+      this.router.navigate(['/seleccion-butaca'], {
+        state: {
+          funcion: this.funcionSeleccionada,
+          pelicula: this.pelicula,
+        },
+      });
     }
+  }
 
-    volver(): void {
-        this.router.navigate(['/cartelera']);
-    }
+  volver(): void {
+    this.router.navigate(['/cartelera']);
+  }
 }
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../pages/axios_service/auth.service';
+import { Router } from '@angular/router';
+import { ApiService } from '../../pages/axios_service/api.service';
 
+interface usuario {
+  nombre: string;
+  apellido: string;
+  email: string;
+  fechaNacimiento: string;
+  nroTelefono: string;
+  tipoCliente: {
+    denominacion: string;
+    descripcion: string;
+  };
+}
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
+  standalone: true,
 })
-export class Header {
+export class Header implements OnInit {
+  userData: usuario | null = null;
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private readonly apiService: ApiService
+  ) {}
+  ngOnInit(): void {
+    this.initialization();
+  }
+
+  async initialization(): Promise<void> {
+    try {
+      this.userData = await this.apiService.getDatosUsuario();
+    } catch (error) {
+      console.error(error);
+      this.errorMessage = 'No se pudo cargar el usuario';
+    }
+  }
 }
