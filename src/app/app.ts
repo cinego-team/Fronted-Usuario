23627498dad30bf3bd31351f17cbfcad298f2691
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-08 21:46:30 -0300

    minifix loading animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TokenTimeoutService } from './services/tokenTimeout.service';
+import { LoadingAnimationComponent } from './shared/loading-animation/loading-animation';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, LoadingAnimationComponent],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
